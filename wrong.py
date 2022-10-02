df0dd185912531cd4b4257d75ea0e1d567e44db4
ycc66104116 <p66104116@gs.ncku.edu.tw>
2022-10-02 16:12:49 +0800

    add minus

--- wrong.py
+++ wrong.py
@@ -2,3 +2,4 @@
 a = eval(input('please enter teh first number: '))
 b = eval(input('please enter the second number: '))
 print("The first number adding the second number is: ",a+b)
+print("The first number minus the second number is: ",a-b)
