fc863d7be6182edd869bb0d87ac7e599bb2bb972
ycc66104116 <p66104116@gs.ncku.edu.tw>
2022-10-02 16:37:50 +0800

    commit without minus

--- wrong.py
+++ wrong.py
@@ -4,3 +4,4 @@ b = eval(input('please enter the second number: '))
 print("The first number adding the second number is: ",a+b)
 print("The first number multiply the second number is: ",a*b)
 print("The first number divide the second number is: ",a/b)
+git merge --continue
